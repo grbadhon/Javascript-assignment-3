c273ad6897564befcd90b6d526b814d7dce08462
badhon <rrafibadhon@gmail.com>
2021-01-22 01:41:34 +0600

    four problems done

--- assignment.js
+++ assignment.js
@@ -8,7 +8,7 @@ function kilometerToMeter(kilometer) {
     if (kilometer < 0) {
         console.log("Distance Can not be negative")
     } else if (kilometer == 0) {
-        // 0 kilometers = 0 meter
+        // 0 kilometer = 0 meter
         return 0;
     } else {
         return kilometer * 1000;
