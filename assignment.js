cb4a639d9f1cc2bae9a773f7460e794e29b79a05
badhon <rrafibadhon@gmail.com>
2021-01-22 01:21:17 +0600

    two promlems done

--- assignment.js
+++ assignment.js
@@ -13,3 +13,30 @@ function kilometerToMeter(kilometer) {
 }
 
 // Kilometer To Meter Conversion End
+
+// Budget Calculator Problem Start 
+
+function budgetCalculator(numberOfClock, numberOfPhone, numberOfLaptop) {
+    // If enter any negative value
+    if (numberOfClock < 0 && numberOfLaptop < 0 && numberOfLaptop < 0) {
+        console.log("Please Give Valid Input");
+    } 
+    // If enter all values 0
+    else if (numberOfClock == 0 && numberOfLaptop == 0 && numberOfLaptop == 0) {
+        console.log("Buy at least one thing to get calculation");
+    } 
+    
+    else {
+        var budget = 0;
+        if (numberOfClock < 0 || numberOfPhone < 0 || numberOfLaptop < 0) {
+            console.log("Number of items can not be negative value");
+
+        } else {
+            budget = (numberOfClock * 50) + (numberOfPhone * 100) + (numberOfLaptop * 500);
+            return budget;
+        }
+    }
+}
+console.log(budgetCalculator(0, 0, 8))
+
+// Budget Calculator Problem End 
